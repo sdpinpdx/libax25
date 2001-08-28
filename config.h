6f656182631c9660e1823d95a7a2d957e22c8573
Craig Small <csmall@enc.com.au>
2001-08-28 00:22:39 +0000

    made final change to changelog

--- config.h
+++ config.h
@@ -1,57 +1,58 @@
 /* config.h.  Generated automatically by configure.  */
 /* config.h.in.  Generated automatically from configure.in by autoheader.  */
-
-/* Define to empty if the keyword does not work.  */
-/* #undef const */
-
-/* Define as the return type of signal handlers (int or void).  */
-#define RETSIGTYPE void
-
-/* Define if the `setpgrp' function takes no argument.  */
-#define SETPGRP_VOID 1
-
-/* Define if you have the ANSI C header files.  */
-#define STDC_HEADERS 1
-
 /* Define if you have a proper netax25/ax25.h header file. */
-#define HAVE_NETAX25_AX25_H 1 
+#define HAVE_NETAX25_AX25_H 1
 
 /* Define if you have a proper netrom/netrom.h header file. */
-#define HAVE_NETROM_NETROM_H 1 
+#define HAVE_NETROM_NETROM_H 1
 
 /* Define if you have a proper netrose/rose.h header file. */
-#define HAVE_NETROSE_ROSE_H 1 
+#define HAVE_NETROSE_ROSE_H 1
 
-/* Define if you have the socket function.  */
-#define HAVE_SOCKET 1
+/* Define if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
 
-/* Define if you have the strdup function.  */
-#define HAVE_STRDUP 1
-
-/* Define if you have the strerror function.  */
-#define HAVE_STRERROR 1
-
-/* Define if you have the strspn function.  */
-#define HAVE_STRSPN 1
-
-/* Define if you have the <fcntl.h> header file.  */
+/* Define if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
 
-/* Define if you have the <sys/file.h> header file.  */
+/* Define if you have the `socket' function. */
+#define HAVE_SOCKET 1
+
+/* Define if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define if you have the `strspn' function. */
+#define HAVE_STRSPN 1
+
+/* Define if you have the <sys/file.h> header file. */
 #define HAVE_SYS_FILE_H 1
 
-/* Define if you have the <sys/ioctl.h> header file.  */
+/* Define if you have the <sys/ioctl.h> header file. */
 #define HAVE_SYS_IOCTL_H 1
 
-/* Define if you have the <unistd.h> header file.  */
+/* Define if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
-/* Define if you have the <zlib.h> header file.  */
+/* Define if you have the <zlib.h> header file. */
 #define HAVE_ZLIB_H 1
 
 /* Name of package */
 #define PACKAGE "libax25"
 
-/* Version number of package */
-#define VERSION "0.0.7"
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
 
+/* Define if the `setpgrp' function takes no argument. */
+/* #undef SETPGRP_VOID */
+
+/* Define if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.0.9"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
